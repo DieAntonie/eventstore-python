2d8609297a1fb8a8d9e9cb63c1d819d358ccace8
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-08-23 16:41:38 +0200

    Finish sql event store

--- sample-app/Edument.CQRS/SqlEventStore.py
+++ sample-app/Edument.CQRS/SqlEventStore.py
@@ -1,85 +1,88 @@
 from IEventStore import IEventStore
+from datetime import datetime
 
 class SqlEventStore(IEventStore):
-    def __init__(self, hostname = 'localhost', username = 'postgres', password = 'masterkey', database = 'postgres'):
-        self.hostname = hostname
-        self.username = username
+    def __init__(self, host = 'localhost', user = 'postgres', password = 'masterkey', dbname = 'postgres'):
+        self.host = host
+        self.user = user
         self.password = password
-        self.database = database
+        self.dbname = dbname
 
     def LoadEventsFor(self, id):
         import psycopg2
-        connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-        try:
-            cur = connection.cursor()
-            cur.execute(f"""
-                SELECT [Type], [Body]
+        with psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname) as connection:
+            db_cursor = connection.cursor()
+            db_cursor.execute(f"""
+                SELECT [Body]
                 FROM [dbo].[Events]
                 WHERE [AggregateId] = {id}
                 ORDER BY [SequenceNumber]
                 """)
 
-            for typeName, data in cur.fetchall() :
-                yield self.DeserializeEvent(typeName, data)
+            for data in db_cursor.fetchall() :
+                yield self.DeserializeEvent(data)
 
-        finally:
-            connection.close()
-
-    def DeserializeEvent(self, typeName, data):
+    def DeserializeEvent(self, data):
+        def json_to_model(json_obj):
+            """
+            Function that takes in a dict and returns a custom object associated with the dict.
+            This function makes use of the "__module__" and "__class__" metadata in the dictionary
+            to know which object type to create.
+            """
+            if "__class__" in json_obj:
+                # Pop ensures we remove metadata from the dict to leave only the instance arguments
+                class_name = json_obj.pop("__class__")
+                # Get the module name from the dict and import it
+                module_name = json_obj.pop("__module__")
+                # We use the built in __import__ function since the module name is not yet known at runtime
+                module = __import__(module_name)
+                # Get the class from the module
+                class_ = getattr(module, class_name)
+                # Use dictionary unpacking to initialize the object
+                model_obj = class_(**json_obj)
+            else:
+                model_obj = json_obj
+            return model_obj
+        
         import json
-        var ser = new XmlSerializer(Type.GetType(typeName))
-        var ms = new MemoryStream(Encoding.UTF8.GetBytes(data))
-        ms.Seek(0, SeekOrigin.Begin)
-        return ser.Deserialize(ms)
+        return json.loads(data, object_hook = json_to_model)
 
-    void SaveEventsFor<TAggregate>(Guid aggregateId, int eventsLoaded, ArrayList newEvents)
-{
-    using (var cmd = new SqlCommand())
-    {
-        // Query prelude.
-        var queryText = new StringBuilder(512);
-        queryText.AppendLine("BEGIN TRANSACTION;");
-        queryText.AppendLine(
-            @"IF NOT EXISTS(SELECT * FROM [dbo].[Aggregates] WHERE [Id] = @AggregateId)
-                    INSERT INTO [dbo].[Aggregates] ([Id], [Type]) VALUES (@AggregateId, @AggregateType);");
-        cmd.Parameters.AddWithValue("AggregateId", aggregateId);
-        cmd.Parameters.AddWithValue("AggregateType", typeof(TAggregate).AssemblyQualifiedName);
+    def SaveEventsFor(self, aggregateId, eventsLoaded, newEvents):
+        # Query prelude.
+        queryText = f"""
+            BEGIN TRANSACTION;
+            IF NOT EXISTS(SELECT * FROM [dbo].[Aggregates] WHERE [Id] = {aggregateId})
+                INSERT INTO [dbo].[Aggregates] ([Id]) VALUES ({aggregateId});
+        """
+        # Add saving of the events.
+        CommitDateTime = datetime.now()
+        for index, e in enumerate(newEvents):
+            queryText += f"""
+                INSERT INTO [dbo].[Events] ([AggregateId], [SequenceNumber], [Body], [Timestamp])
+                    VALUES('{aggregateId}', {eventsLoaded + index}, '{self.SerializeEvent(e)}', '{CommitDateTime}');
+                """
+        # Add commit.
+        queryText += "COMMIT;"
+        # Execute the update.
+        import psycopg2
+        with psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname) as connection:
+            db_cursor = connection.cursor()
+            db_cursor.execute(queryText)
 
-        // Add saving of the events.
-        cmd.Parameters.AddWithValue("CommitDateTime", DateTime.UtcNow);
-        for (int i = 0; i < newEvents.Count; i++)
-        {
-            var e = newEvents[i];
-            queryText.AppendFormat(
-                @"INSERT INTO [dbo].[Events] ([AggregateId], [SequenceNumber], [Type], [Body], [Timestamp])
-                    VALUES(@AggregateId, {0}, @Type{1}, @Body{1}, @CommitDateTime);",
-                eventsLoaded + i, i);
-            cmd.Parameters.AddWithValue("Type" + i.ToString(), e.GetType().AssemblyQualifiedName);
-            cmd.Parameters.AddWithValue("Body" + i.ToString(), SerializeEvent(e));
-        }
+    def SerializeEvent(self, event):
+        def model_to_json(model_obj):
+            """
+            A function takes in a custom object and returns a dictionary representation of the object.
+            This dict representation includes meta data such as the object's module and class names.
+            """
+            #  Populate the dictionary with object meta data 
+            json_obj = {
+                "__class__": model_obj.__class__.__name__,
+                "__module__": model_obj.__module__
+            }
+            #  Populate the dictionary with object properties
+            json_obj.update(model_obj.__dict__)
+            return json_obj
 
-        // Add commit.
-        queryText.Append("COMMIT;");
-
-        // Execute the update.
-        using (var con = new SqlConnection(connectionString))
-        {
-            con.Open();
-            cmd.Connection = con;
-            cmd.CommandText = queryText.ToString();
-            cmd.CommandType = CommandType.Text;
-            cmd.ExecuteNonQuery();
-        }
-    }
-}
-
-    string SerializeEvent(object obj)
-{
-    var ser = new XmlSerializer(obj.GetType());
-    var ms = new MemoryStream();
-    ser.Serialize(ms, obj);
-    ms.Seek(0, SeekOrigin.Begin);
-    return new StreamReader(ms).ReadToEnd();
-}
-}
-}
+        import json
+        return json.dumps(event, default = model_to_json)
