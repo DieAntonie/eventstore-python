8eca6a6757f32c3b73379a3873b65105f740e986
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-10-04 22:16:43 +0200

    Complete simple walktrough

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from sample_app.Edument_CQRS.MessageDispatcher import MessageDispatcher
 from sample_app.Edument_CQRS.SqlEventStore import SqlEventStore
 from sample_app.CafeReadModels.OpenTabs import OpenTabs
 from sample_app.CafeReadModels.ChefTodoList import ChefTodoList
-from sample_app.Cafe.Tab.TabAggregate import TabAggregate
+from sample_app.Cafe.Tab.CloseTab import CloseTab
 from sample_app.Cafe.Tab.MarkDrinksServed import MarkDrinksServed
 from sample_app.Cafe.Tab.MarkFoodPrepared import MarkFoodPrepared
 from sample_app.Cafe.Tab.MarkFoodServed import MarkFoodServed
 from sample_app.Cafe.Tab.OpenTab import OpenTab
 from sample_app.Cafe.Tab.PlaceOrder import PlaceOrder
+from sample_app.Cafe.Tab.TabAggregate import TabAggregate
 from sample_app.Events.Tab.Shared import OrderedItem
 import uuid
 
@@ -30,43 +31,117 @@ Dispatcher.ScanInstance(ChefTodoListQueries)
 # 
 Tab_Key = uuid.uuid1()
 
-# We start by opening a tab for table 3, served by Chris.
-open_a_tab = OpenTab(Id=Tab_Key, TableNumber=3, Waiter="Chris")
+# Print State
+print("### Part 0: Current state")
+print(f"-->  Active Tables Numbers are: {OpenTabQueries.ActiveTableNumbers()}")
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
 
-# We send the command to the dispatcher, to apply to the appropriate handlers.
+# We start by opening a tab for table 3, served by Chris.
+print("### Part 1: Open a new Tab on table 3 for waiter Chris")
+open_a_tab = OpenTab(Id=Tab_Key, TableNumber=3, Waiter="Chris")
 Dispatcher.SendCommand(open_a_tab)
 
+# Print State
+print(f"-->  Active Tables Numbers are: {OpenTabQueries.ActiveTableNumbers()}")
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
+print(f"-->  Tab ID for table is: {OpenTabQueries.TabIdForTable(3)}")
+print(f"-->  Tab for table is: {OpenTabQueries.TabForTable(3)}")
+print(f"-->  Invoice for table is: {OpenTabQueries.InvoiceForTable(3)}")
+
 # Decide on the order items
-my_steak = OrderedItem(
+print("### Part 2: Order 4 Items [Steak, Ribs, Beer, Beer]")
+my_steak_order = OrderedItem(
     MenuNumber=2,
     Description="Steak",
     IsDrink=False,
     Price=100.00
     )
 
-your_ribs = OrderedItem(
+your_ribs_order = OrderedItem(
     MenuNumber=1,
     Description="Ribs",
     IsDrink=False,
     Price=120.00
     )
 
-my_beer = OrderedItem(
+my_beer_order = OrderedItem(
     MenuNumber=40,
     Description="Beer",
-    IsDrink=False,
+    IsDrink=True,
     Price=25.00
     )
 
-your_beer = OrderedItem(
+your_beer_order = OrderedItem(
     MenuNumber=40,
     Description="Beer",
-    IsDrink=False,
+    IsDrink=True,
     Price=25.00
     )
 
 # Place order for decided on order items
-place_an_order = PlaceOrder(Id=Tab_Key, Items=[my_steak, your_ribs, my_beer, your_beer])
+place_an_order = PlaceOrder(Id=Tab_Key, Items=[my_steak_order, your_ribs_order, my_beer_order, your_beer_order])
 Dispatcher.SendCommand(place_an_order)
 
+# Print State
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
+print(f"-->  Tab ID for table is: {OpenTabQueries.TabIdForTable(3)}")
+print(f"-->  Tab for table is: {OpenTabQueries.TabForTable(3)}")
+print(f"-->  Invoice for table is: {OpenTabQueries.InvoiceForTable(3)}")
 
+print("### Part 3: Serve the beers")
+drinks_menu_numbers = [waiter_item[3] for waiter_item in todo_list if waiter_item.get(3)][0]
+serve_drinks = MarkDrinksServed(Id=Tab_Key, MenuNumbers=[drinks.MenuNumber for drinks in drinks_menu_numbers])
+Dispatcher.SendCommand(serve_drinks)
+
+# Print State
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
+print(f"-->  Tab ID for table is: {OpenTabQueries.TabIdForTable(3)}")
+print(f"-->  Tab for table is: {OpenTabQueries.TabForTable(3)}")
+print(f"-->  Invoice for table is: {OpenTabQueries.InvoiceForTable(3)}")
+
+print("### Part 4: Prepare the food")
+chef_todo_list_group = ChefTodoListQueries.GetTodoList()[0]
+prepare_food = MarkFoodPrepared(Id=Tab_Key, MenuNumbers=[chef_item.MenuNumber for chef_item in chef_todo_list_group.Items])
+Dispatcher.SendCommand(prepare_food)
+
+# Print State
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
+print(f"-->  Tab ID for table is: {OpenTabQueries.TabIdForTable(3)}")
+print(f"-->  Tab for table is: {OpenTabQueries.TabForTable(3)}")
+print(f"-->  Invoice for table is: {OpenTabQueries.InvoiceForTable(3)}")
+
+print("### Part 5: Serve the food")
+food_menu_numbers = [waiter_item[3] for waiter_item in todo_list if waiter_item.get(3)][0]
+serve_food = MarkFoodServed(Id=Tab_Key, MenuNumbers=[food.MenuNumber for food in food_menu_numbers])
+Dispatcher.SendCommand(serve_food)
+
+# Print State
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
+print(f"-->  Tab ID for table is: {OpenTabQueries.TabIdForTable(3)}")
+print(f"-->  Tab for table is: {OpenTabQueries.TabForTable(3)}")
+print(f"-->  Invoice for table is: {OpenTabQueries.InvoiceForTable(3)}")
+
+print("### Part 6: Close the tab")
+amount_to_pay = sum([invoice_item.Price for invoice_item in OpenTabQueries.InvoiceForTable(3).Items]) + 50
+close_tab = CloseTab (Id=Tab_Key, AmountPaid=amount_to_pay)
+Dispatcher.SendCommand(close_tab)
+
+# Print State
+todo_list = OpenTabQueries.TodoListForWaiter("Chris")
+print(f"-->  Todo List For Waiter is: {todo_list}")
+print(f"-->  Todo List For Chef is: {ChefTodoListQueries.GetTodoList()}")
+print(f"-->  Tab ID for table is: {OpenTabQueries.TabIdForTable(3)}")
+print(f"-->  Tab for table is: {OpenTabQueries.TabForTable(3)}")
+print(f"-->  Invoice for table is: {OpenTabQueries.InvoiceForTable(3)}")
