c41f1852f780551bba3148d81026c4fc765b4ead
quantonie <antonie@quantsolutions.co.za>
2019-10-28 13:50:30 +0200

    Include dungeonsdragons test suite

--- runner.py
+++ runner.py
@@ -3,13 +3,19 @@ import unittest
 
 # import your test modules
 from src.CafeTests import TabTests
+from src.DungeonsDragonsTests import (
+    CharacterTests,
+    EnvironmentTests
+)
 
 # initialize the test suite
 loader = unittest.TestLoader()
-suite  = unittest.TestSuite()
+suite = unittest.TestSuite()
 
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(TabTests))
+suite.addTests(loader.loadTestsFromModule(CharacterTests))
+suite.addTests(loader.loadTestsFromModule(EnvironmentTests))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
