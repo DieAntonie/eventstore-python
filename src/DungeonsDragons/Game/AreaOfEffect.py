a1d87bc65fd7939877a6062ae351726a9700db3a
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2019-11-01 07:29:08 +0200

    Add all Area of Effects

--- src/DungeonsDragons/Game/AreaOfEffect.py
+++ src/DungeonsDragons/Game/AreaOfEffect.py
@@ -4,11 +4,26 @@ class AreaOfEffect(object):
     pass
 
 
+class Cone(AreaOfEffect):
+    def __init__(self, length):
+        pass
+
+
+class Cube(AreaOfEffect):
+    def __init__(self, length):
+        pass
+
+
+class Cylinder(AreaOfEffect):
+    def __init__(self, radius, height):
+        pass
+
+
 class Line(AreaOfEffect):
     def __init__(self, width, length):
         pass
 
 
-class Cone(AreaOfEffect):
-    def __init__(self, length):
+class Sphere(AreaOfEffect):
+    def __init__(self, radius):
         pass
