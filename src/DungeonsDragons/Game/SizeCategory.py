47778bfc225fb89a03030383f3748e08ace2d3eb
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2020-03-15 21:14:31 +0200

    Adjust size category values

--- src/DungeonsDragons/Game/SizeCategory.py
+++ src/DungeonsDragons/Game/SizeCategory.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class SizeCategory(Enum):
-    Tiny = (2.5)
-    Small = (5)
-    Medium = (5)
-    Large = (10)
-    Huge = (15)
-    Gargantuan = (20)
+    Tiny = "xs"
+    Small = "s"
+    Medium = "m"
+    Large = "l"
+    Huge = "xl"
+    Gargantuan = "xxl"
 
     def __init__(self, dimension):
         self.dimension = dimension
